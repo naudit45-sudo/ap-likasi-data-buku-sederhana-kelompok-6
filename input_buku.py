9a07f6dbcbf179720e52a7814306cc22bce46c29
Mini Project Kelompok 6 <usaisamuda328@gmail.com>
2026-04-22 11:45:43 +0700

    baru

---
+++ input_buku.py
@@ -0,0 +1,26 @@
+class InputBuku:
+
+    def input_nama_petugas(self):
+        return input("Masukkan nama petugas: ")
+
+    def input_data_buku(self):
+        daftar_buku = []
+
+        for i in range(3):
+            print(f"\nBuku ke-{i+1}")
+            judul = input("Masukkan judul buku: ")
+
+            # validasi angka
+            while True:
+                try:
+                    halaman = int(input("Masukkan jumlah halaman: "))
+                    break
+                except ValueError:
+                    print("Input harus berupa angka!")
+
+            daftar_buku.append({
+                "judul": judul,
+                "halaman": halaman
+            })
+
+        return daftar_buku
